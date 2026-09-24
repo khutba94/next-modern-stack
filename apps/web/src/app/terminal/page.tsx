c86fd4952ca2ea35b7ee347e64511fcdf29ee292
khutba94 <khutba94@gmail.com>
2026-09-24 12:11:39 +0300

    Add new feature

--- apps/web/src/app/terminal/page.tsx
+++ apps/web/src/app/terminal/page.tsx
@@ -2,6 +2,7 @@ import PageWrapper from "@/modules/shared/page-wrapper";
 import TerminalContent from "@/modules/terminal/terminal-content";
 
 export default function TerminalPage() {
+  console.log("TerminalPage");
   return (
     <PageWrapper>
       <TerminalContent />
